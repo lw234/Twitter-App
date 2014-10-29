bebbb221cf4874fb0ea426177d244b453f28a9c7
Lynn <lynn291991@yahoo.com>
2014-10-28 23:10:45 -0400

    Add Get and post request

--- Challenge3/index.php
+++ Challenge3/index.php
@@ -4,40 +4,59 @@ error_reporting(E_ALL | E_STRICT);
 
 //require_once('TwitterAPIExchange.php');
 
+
+require 'DisplayRecord.php';
 require ('autoloader.php');
 spl_autoload_register('Autoloader::loader');
 
 
  
 /** Set access tokens here - see: https://dev.twitter.com/apps/ **/
-$settings = array(
-    'oauth_access_token' => "64633572-gzdrjADLeG45QCJHxC9JvFLp1RB6RIqV22niTMfXm",
-    'oauth_access_token_secret' => "FbmRD5oOXEQ3KfYONorFYb3clVmyXdYzY7sofPY1mqIOk",
-    'consumer_key' => "r4TTjzxoLI6bOrx0WyB4AzUqk",
-    'consumer_secret' => "h0VUg2P19jpe3np4hu9Yy7gdQ2dBzMud2jRisYcSruNQkqu8jv"
-    );
-	
+
+
 $url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
+//$url= "https://api.twitter.com/1.1/statuses/home_timeline.json";
 
 $requestMethod = "GET"; 
+//if (isset($_GET['user']))  {$user = $_GET['user'];}  else {$user  = "iagdotme";} 
+//if (isset($_GET['count'])) {$user = $_GET['count'];} else {$count = 20;}
+$getfield = "?screen_name=iagdotme&count=20";
 
-if (isset($_GET['user']))  {$user = $_GET['user'];}  else {$user  = "iagdotme";}
-if (isset($_GET['count'])) {$user = $_GET['count'];} else {$count = 20;}
-$getfield = "?screen_name=$user&count=$count";
-$twitter = new Classes\Twitter\TwitterAPIExchange($settings);
+//$url = "https://api.twitter.com/1.1/followers/list.json";
+//$requestMethod = "GET"; 
+//$getfield = "?screen_name=$user&count=$count";
+
+
+$twitter = new \Classes\Twitter\TwitterAPIExchange($settings);
 $string = json_decode($twitter->setGetfield($getfield)->buildOauth($url, $requestMethod)->performRequest(),$assoc = TRUE);
-//$string =\Classes\html\DisplayRecord::Dtable($string);
-//if($string["errors"][0]["message"] != "") {echo "<h3>Sorry, there was a problem.</h3><p>Twitter returned the following error message:</p><p><em>".$string[errors][0]["message"]."</em></p>";exit();}
-foreach($string as $items)
-    {
-        echo "Time and Date of Tweet: ".$items['created_at']."<br />";
-        echo "Tweet: ". $items['text']."<br />";
-        echo "Tweeted by: ". $items['user']['name']."<br />";
-        echo "Screen name: ". $items['user']['screen_name']."<br />";
-        echo "Followers: ". $items['user']['followers_count']."<br />";
-        echo "Friends: ". $items['user']['friends_count']."<br />";
-        echo "Listed: ". $items['user']['listed_count']."<br /><hr />";
-    }
+$Tuser = DisplayRecord::print_user_timeline($string);
+
+$url= "https://api.twitter.com/1.1/statuses/home_timeline.json";
+$requiresMethod = "GET";
+$getfield = "?screen_name=iagdotme&count=20";
+$string = json_decode($twitter->setGetfield($getfield)->buildOauth($url, $requestMethod)->performRequest(),$assoc = TRUE);
+$UserF = DisplayRecord::print_user_timeline($string);
+
+/**$url="https://api.twitter.com/1.1/followers/ids.json";
+$requiresMethod = "GET";
+$getfield = "?screen_name=kunpooka&count=20";
+$string = json_decode($twitter->setGetfield($getfield)->buildOauth($url, $requestMethod)->performRequest(),$assoc = TRUE);
+$UserF = DisplayRecord::print_home_timeline($string); 
+**/
+
+$url="https://api.twitter.com/1.1/statuses/update.json";
+$requestMethod = "POST"; 
+$getfield = "?screen_name=iagdotme&count=20";
+/** POST fields required by the URL above. See relevant docs as above **/
+$postfields = array(
+    'screen_name' => 'kunpooka', 
+    'status' => 'Doing Hw for IS218!!!'
+    );
+$twitter = new \Classes\Twitter\TwitterAPIExchange($settings);
+$string = json_decode($twitter->setPostfields($postfields)->buildOauth($url, $requestMethod)->performRequest(),$assoc = TRUE);
+$UserF = DisplayRecord::print_home_timeline($string); 
+
+ 
 ?>
 
 
