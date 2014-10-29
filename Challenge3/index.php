77385ca7f96889feaa40b6f27edbc2e0dfbfb7ac
Lynn <lynn291991@yahoo.com>
2014-10-29 17:04:51 -0400

    use TwitterAPI to update twitter statuses

--- Challenge3/index.php
+++ Challenge3/index.php
@@ -13,7 +13,6 @@ spl_autoload_register('Autoloader::loader');
  
 /** Set access tokens here - see: https://dev.twitter.com/apps/ **/
 
-
 $url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
 //$url= "https://api.twitter.com/1.1/statuses/home_timeline.json";
 
@@ -37,24 +36,31 @@ $getfield = "?screen_name=iagdotme&count=20";
 $string = json_decode($twitter->setGetfield($getfield)->buildOauth($url, $requestMethod)->performRequest(),$assoc = TRUE);
 $UserF = DisplayRecord::print_user_timeline($string);
 
-/**$url="https://api.twitter.com/1.1/followers/ids.json";
-$requiresMethod = "GET";
-$getfield = "?screen_name=kunpooka&count=20";
-$string = json_decode($twitter->setGetfield($getfield)->buildOauth($url, $requestMethod)->performRequest(),$assoc = TRUE);
-$UserF = DisplayRecord::print_home_timeline($string); 
-**/
+//$url="https://api.twitter.com/1.1/followers/ids.json";
+//$requiresMethod = "GET";
+//$getfield = "?screen_name=kunpooka&count=20";
+//$string = json_decode($twitter->setGetfield($getfield)->buildOauth($url, $requestMethod)->performRequest(),$assoc = TRUE);
+//$UserF = DisplayRecord::print_home_timeline($string); 
 
 $url="https://api.twitter.com/1.1/statuses/update.json";
 $requestMethod = "POST"; 
-$getfield = "?screen_name=iagdotme&count=20";
-/** POST fields required by the URL above. See relevant docs as above **/
+
+//POST fields required by the URL above. See relevant docs as above 
 $postfields = array(
-    'screen_name' => 'kunpooka', 
-    'status' => 'Doing Hw for IS218!!!'
+    
+    'status' => 'Using TwitterAPI to tweet!!'
     );
 $twitter = new \Classes\Twitter\TwitterAPIExchange($settings);
-$string = json_decode($twitter->setPostfields($postfields)->buildOauth($url, $requestMethod)->performRequest(),$assoc = TRUE);
-$UserF = DisplayRecord::print_home_timeline($string); 
+$string = json_decode($twitter->setGetfield($getfield)->buildOauth($url, $requestMethod)->performRequest(),$assoc = TRUE);
+$UserF = DisplayRecord::printPost($string);
+/**
+echo $twitter->buildOauth($url, $requestMethod)
+             ->setPostfields($postfields)
+             ->performRequest();
+  **/   
+//$string = json_decode($twitter->setPostfields($postfields)->buildOauth($url, $requestMethod)->performRequest(),$assoc = TRUE);
+
+//$UserF = DisplayRecord::print_home_timeline($string); 
 
  
 ?>
