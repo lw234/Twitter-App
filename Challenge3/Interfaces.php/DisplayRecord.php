ce855bc4df8111152f35204782ae82b32f71af8d
Lynn <lynn291991@yahoo.com>
2014-10-31 23:49:15 -0400

    add Interfaces

---
+++ Challenge3/Interfaces.php/DisplayRecord.php
@@ -0,0 +1,9 @@
+<?php 
+namespace Interfaces;
+
+interface DisplayRecord_Template{
+	public static function print_user_timeline($string);
+	public static function printPost($string);
+	public static function printFollowerslist($string);
+}
+ ?>
